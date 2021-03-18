85ec5a8a0b1cc875f03eef5fe57f99471368ae30
Barbara Barabas <bbarbara092@gmail.com>
2021-03-18 13:02:33 +0100

    use apache server

--- get-to-know-lara-backend/app/Http/Controllers/UserController.php
+++ get-to-know-lara-backend/app/Http/Controllers/UserController.php
@@ -2,19 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     function register(Request $request)
     {
-        $user = new User;
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->password = Hash::make( $request->input('password'));
-        $user->save();
-        return $user;
+
+        User::firstOrCreate([ "name" => $request->get('name'), "email" => $request->get('userEmail'), "password" => Hash::make( $request->get('userPassword'))]);
+        return response()->json([
+            'status_code' => 200,
+            'message' => 'User created successfully'
+        ]);
     }
+
+    function login(Request $request)
+    {
+        try{
+            $validation = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required']);
+        }
+        catch (\Exception $exception){
+            return response()->json("error", Response::HTTP_BAD_REQUEST);
+        }
+
+
+        $user= User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['These credentials do not match our records.']
+            ], 404);
+        }
+
+        $token = $user->createToken('authToken')->plainTextToken;
+        return response()->json([
+                'user' => $user,
+                'token' => $token,
+        ]);
+
+    }
+
+
+
+
+
 }
+
