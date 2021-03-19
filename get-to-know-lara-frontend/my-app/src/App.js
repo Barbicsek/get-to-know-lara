a3b43fa9411f920a32ae7d06e4dfa8230ab1a9a2
Barbara Barabas <bbarbara092@gmail.com>
2021-03-19 17:50:19 +0100

    add new route

--- get-to-know-lara-frontend/my-app/src/App.js
+++ get-to-know-lara-frontend/my-app/src/App.js
@@ -4,7 +4,8 @@ import React from "react";
 import Registration from "./components/Registration";
 import Login from "./components/Login";
 import Inbox from "./components/Inbox";
-
+import EmailsSent from "./components/EmailsSent";
+import ComposeMail from "./components/ComposeMail";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route exact path="/registration" children={<Registration />} />
         <Route exact path="/login" children={<Login />} />
         <Route exact path="/mail/inbox" children={<Inbox />} />
+        <Route exact path="/mail/sent" children={<EmailsSent />} />
+        <Route exact path="/mail/compose" children={<ComposeMail />} />
       </Router>
       
     </div>
