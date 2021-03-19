7ee1d76f5209020f8358ed54299b4d0534ef3c9a
Barbara Barabas <bbarbara092@gmail.com>
2021-03-19 17:48:08 +0100

    add logut, some conditions to the appearing

--- get-to-know-lara-frontend/my-app/src/components/NavBar.js
+++ get-to-know-lara-frontend/my-app/src/components/NavBar.js
@@ -1,53 +1,53 @@
 import React from 'react';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-import {makeStyles} from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import {useHistory} from 'react-router-dom';
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    background: '#F6F6F6',
-    flexGrow: 1,
-    marginBottom: '5px',
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  navbarStyle: {
-    background: '#red',
-  },
-  img: {
-    width: 150,
-    marginTop: 5,
-  },
-}));
 
 export default function MenuAppBar() {
-  const classes = useStyles();
+
+  const user = JSON.parse(sessionStorage.getItem('user'));
+  const history = useHistory();
+
+  function logOut() {
+    sessionStorage.clear();
+    history.push('/')
+
+  }
+
 
   return (
-    <div className={classes.root}>
-      <AppBar className={classes.navbarStyle} position="static">
-        <Toolbar>
-          <div>
-            <Button component={Link} to="/" color="inherit">
-              Home
-            </Button>
-            <Button component={Link} to="/login" color="inherit">
-              Login
-            </Button>
-            <Button component={Link} to="/registration" color="inherit">
-              Registration
-            </Button>
-          </div>
-        </Toolbar>
-      </AppBar>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Nav className="mr-auto nav_bar_wrapper">
+          {
+            sessionStorage.getItem('token') ?
+            <>
+              <Link to="/mail/sent">Sent Mails</Link>
+              <Link to="/mail/compose">Compose mails</Link>
+            </>
+            :
+            <>
+              <Link to="/login">Login  </Link>
+    
+              <Link to="/registration">Registration  </Link>
+            </>
+          }
+        </Nav>
+        {sessionStorage.getItem('user') ?
+                <Nav>
+          
+                <NavDropdown title={user && user["name"]}>
+                  <NavDropdown.Item onClick={logOut}>Logut</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              :null
+
+        }
+
+      </Navbar>
+
     </div>
   );
 }
