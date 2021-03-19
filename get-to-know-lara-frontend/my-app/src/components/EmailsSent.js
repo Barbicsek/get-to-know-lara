7d663d032d869da7704379dad5d00e4f35445c04
Barbara Barabas <bbarbara092@gmail.com>
2021-03-19 17:49:41 +0100

    display all of the emails sent by the user

---
+++ get-to-know-lara-frontend/my-app/src/components/EmailsSent.js
@@ -0,0 +1,92 @@
+import React, {useState, useEffect} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+
+function Inbox() {
+
+    const classes = useStyles();
+
+    const [mails, setMails] = useState({})
+    console.log(mails)
+
+    let result;
+    const fetchUser = async () => {
+        await fetch('http://localhost/get-to-know-lara/get-to-know-lara-backend/public/api/mail/sent', {
+            method : 'GET',
+            headers : {
+                'Content-Type' : 'application/json',
+                'Accept' : 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+
+            },
+        })
+            .then((response) => {
+                return result = response.json();
+
+            })
+            .then((data) => {
+                setMails(data['mail'])
+            })
+
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+
+    return (
+
+
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>subject</TableCell>
+                        <TableCell align="right">Message</TableCell>
+                        <TableCell align="right">Sent</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {Object.keys(mails).map(key => (
+
+                        <TableRow key={mails[key].subject}>
+                            <TableCell component="th" scope="row">
+                                {mails[key].subject}
+                            </TableCell>
+                            <TableCell align="right">{mails[key].message}</TableCell>
+                            <TableCell align="right">{mails[key].sent}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+export default Inbox;
